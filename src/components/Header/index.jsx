55415fb4379920cd2ef729041c1a7fc1990255df
Ismael Costa <ismael.costa@clicksign.com>
2023-01-03 16:59:08 -0300

    feat: header mobile finished

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,20 @@
 import style from "./style.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass, faBars } from "@fortawesome/free-solid-svg-icons";
+import headerImageMobile from "../../assets/header/headerImageMobile.png";
 
 const Header = () => {
-  return <header className={style.header}>
-    <button>...</button>
-    <div>imagem</div>
-    <div><FontAwesomeIcon icon="fa-light fa-magnifying-glass" /></div>
-  </header>;
+  return (
+    <header className={style.header}>
+      <button className={style.hamburguerButton}>
+        <FontAwesomeIcon icon={faBars} />
+      </button>
+      <img src={headerImageMobile} alt="Logo" />
+      <div>
+        <FontAwesomeIcon icon={faMagnifyingGlass} />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
