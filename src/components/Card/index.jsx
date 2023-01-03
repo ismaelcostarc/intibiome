4e5e7874d3019b6255fd80d542b380dc63687897
Ismael Costa <ismael.costa@clicksign.com>
2023-01-03 18:23:16 -0300

    feat: mobile version of products finished

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,7 +1,20 @@
-import style from "./style.module.css";
+import style from "./style.module.scss";
 
-const Card = ({title, color, description, cover}) => {
-  return <div className={style.container}>Card</div>;
+const Card = ({ title, color, description, cover }) => {
+  return (
+    <div className={style.container}>
+      <img src={cover} alt="Cover" />
+      <div className={`${style.description} text text-3`}>{description}</div>
+      {title && (
+        <div
+          style={{ backgroundColor: color }}
+          className={`${style.footer} text text-4`}
+        >
+          {title}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Card;
